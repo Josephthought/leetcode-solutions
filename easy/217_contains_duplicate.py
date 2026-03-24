45bf5b03dbc4869f70032a16682531de5ea82cee
Joseph Muia <muiajoseph318@gmail.com>
2026-03-24 22:39:19 +0300

    Update 217_contains_duplicate.py

--- easy/217_contains_duplicate.py
+++ easy/217_contains_duplicate.py
@@ -3,6 +3,7 @@
 # Time: 0(n) |Space: 0(n)
 # Solved: March 2026
 
+# solution 1 - explicit loop
 class Solution:
   def containsDuplicate(self, nums):
 
@@ -15,3 +16,9 @@ class Solution:
         seen.add(num)
 
     return False
+
+# solution 2 - one linear (impressive)
+
+class Solution:
+    def containsDuplicate(self, nums):
+        return len(nums) != len(set(nums)) 
